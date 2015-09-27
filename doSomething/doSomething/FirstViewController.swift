c0e8928eb1c53f5e2fbc80eede8340772ec457ce
Evangeline Liu <eliu@MacBook-Pro-6.local>
2015-09-26 19:36:11 -0500

    Added app template

---
+++ doSomething/doSomething/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  doSomething
+//
+//  Created by Evi Liu on 9/26/15.
+//  Copyright © 2015 CLEP. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
